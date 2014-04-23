c1b1fb709b017ce0957fa5d65d1f944443ba4de6
David Arceneaux <david@davidthemachine.org>
2014-04-23 11:23:51 -0700

    2 new modules; more refinement
    use un-alias form for a lot of commands; get a new hg extension; use
    more version and existence checks, so the softwares don't have to work
    so hard

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # only idempotent commands here so far
+# TODO: translate to ansible
 
-mkdir -p ~/contrib ~/.vim/{syntax,bundle} ~/.config/powerline
+\mkdir -p ~/contrib ~/.vim/{syntax,bundle} ~/.config/powerline
 chmod 600 ~/dotfiles/sshconfig
 
 dotfiles=".vimrc .bashrc .zshrc .profile .bash_profile .hgrc .gitconfig .tmux.conf .Xmodmap .pandoc"
@@ -31,14 +32,14 @@ grab_git_diffall() {
   if [ ! -d ~/contrib/git-diffall/.git/ ]; then
     git clone https://github.com/thenigan/git-diffall.git ~/contrib/git-diffall
   else
-    cd ~/contrib/git-diffall
+    \cd ~/contrib/git-diffall
     git up
-    cd -
+    \cd -
   fi
 }
 
 build_wp_cli() {
-  cd ~/contrib/wp-cli
+  \cd ~/contrib/wp-cli
   git checkout "tags/v${1}"
   git submodule update --init --recursive
   if [ -f ~/contrib/wp-cli/composer.lock ]; then
@@ -47,8 +48,8 @@ build_wp_cli() {
     composer install
   fi
   # TODO build phar
-  sudo rm /usr/local/bin/wp && \
-    sudo ln -s ~/contrib/wp-cli/bin/wp /usr/local/bin/wp
+  \sudo \rm -f /usr/local/bin/wp && \
+    \sudo \ln -s ~/contrib/wp-cli/bin/wp /usr/local/bin/wp
 }
 
 grab_wp_cli() {
@@ -68,35 +69,64 @@ grab_hg_prompt() {
   if [ ! -d ~/contrib/hg-prompt/.hg/ ]; then
     hg clone https://bitbucket.org/sjl/hg-prompt/ ~/contrib/hg-prompt/
   else
-    cd ~/contrib/hg-prompt
+    \cd ~/contrib/hg-prompt
     hg pull -u
-    cd -
+    \cd -
+  fi
+}
+
+grab_hgcfg() {
+  if [ ! -d ~/contrib/hgcfg/.hg/ ]; then
+    hg clone https://bitbucket.org/bmearns/hgcfg ~/contrib/hgcfg
+  else
+    \cd ~/contrib/hgcfg
+    hg pull -u
+    \cd -
   fi
 }
 
 grab_powerline() {
   echo "installing/upgrading Powerline"
-  sudo $PIP install -U https://github.com/Lokaltog/powerline/tarball/develop
+  \sudo $PIP install -U https://github.com/Lokaltog/powerline/tarball/develop
 
   powerline_path=$(python -c 'import pkgutil; print pkgutil.get_loader("powerline").filename')
   if [ ! -d ~/.config/powerline ]; then
-    cp -R ${powerline_path}/config_files/* ~/.config/powerline
+    \cp -R ${powerline_path}/config_files/* ~/.config/powerline
   fi
   if [ ! -f ~/.config/powerline/powerline.conf ]; then
-    ln -s \
+    \ln -s \
       ${powerline_path}/bindings/tmux/powerline.conf \
       ~/.config/powerline/powerline.conf
   fi
 }
 
 grab_s3cmd() {
-  echo "installing/upgrading s3cmd"
-  sudo $PIP install -U https://github.com/s3tools/s3cmd/archive/master.zip
+  VER='s3cmd version 1.5.0-beta1'
+  CURR_VER=$(s3cmd --version 2>&1)
+  if [ "${CURR_VER}" != "${VER}"]; then
+    echo "installing/upgrading s3cmd"
+    \sudo $PIP install -U https://github.com/s3tools/s3cmd/archive/master.zip
+  else
+    echo "s3cmd currently installed and up to date"
+  fi
 }
 
+grab_composer() {
+  if ! command -v composer >/dev/null 2>&1 ; then
+    \cd /tmp
+    \curl -sS https://getcomposer.org/installer | php -d "allow_url_fopen=On"
+    \sudo \mv composer.phar /usr/local/bin/composer
+    \cd -
+  else
+    sudo composer self-update
+  fi
+}
+
+grab_composer
 grab_giteditor
 grab_git_diffall
 grab_neobundle
+grab_hgcfg
 grab_hg_prompt
 grab_wp_cli
 grab_powerline
