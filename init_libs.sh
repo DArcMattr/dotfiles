a28cf06eabdf127e3f9f9ad3d24c8c3b374c1eea
David Arceneaux <david@davidthemachine.org>
2015-02-12 10:30:04 -0800

    +grab_icdiff; slash-ify some commands

--- init_libs.sh
+++ init_libs.sh
@@ -2,33 +2,33 @@ PIP=`which pip`
 
 grab_neobundle() {
   if [ ! -d ~/.vim/bundle/neobundle.vim/.git/ ]; then
-    git clone http://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    \git clone http://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
   else
-    cd ~/.vim/bundle/neobundle.vim
-    git up
-    cd -
+    \cd ~/.vim/bundle/neobundle.vim
+    \git up
+    \cd -
   fi
 }
 
 grab_giteditor() {
   if [ ! -d ~/contrib/giteditor/.git/ ]; then
-    git clone https://github.com/dharrigan/giteditor.git ~/contrib/giteditor
+    \git clone https://github.com/dharrigan/giteditor.git ~/contrib/giteditor
   else
-    cd ~/contrib/giteditor
-    git up
-    cd -
+    \cd ~/contrib/giteditor
+    \git up
+    \cd -
   fi
 }
 
 build_wp_cli() {
   \cd ~/contrib/wp-cli
-  git up
-  git checkout "tags/v${1}"
-  git submodule update --init --recursive
+  \git up
+  \git checkout "tags/v${1}"
+  \git submodule update --init --recursive
   if [ -f ~/contrib/wp-cli/composer.lock ]; then
-    composer update
+    \composer update
   else
-    composer install
+    \composer install
   fi
   # TODO build phar
   \sudo \rm -f /usr/local/bin/wp && \
@@ -50,19 +50,19 @@ grab_sassc() {
   fi
 
   if [ ! -d ~/contrib/sassc/.git/ ]; then
-    git clone https://github.com/hcatlin/sassc ~/contrib/sassc
+    \git clone https://github.com/hcatlin/sassc ~/contrib/sassc
   else
-    cd ~/contrib/sassc
-    git up
-    cd -
+    \cd ~/contrib/sassc
+    \git up
+    \cd -
   fi
   echo "you will need to compile these yourself"
 }
 
 grab_wp_cli() {
-  VER='0.17.0'
+  VER='0.18.0'
   if [ ! -d ~/contrib/wp-cli/.git/ ]; then
-    git clone --recursive https://github.com/wp-cli/wp-cli.git ~/contrib/wp-cli/
+    \git clone --recursive https://github.com/wp-cli/wp-cli.git ~/contrib/wp-cli/
     build_wp_cli ${VER}
   else
     CURR_VER=$(~/contrib/wp-cli/bin/wp --version)
@@ -74,20 +74,20 @@ grab_wp_cli() {
 
 grab_hg_prompt() {
   if [ ! -d ~/contrib/hg-prompt/.hg/ ]; then
-    hg clone https://bitbucket.org/sjl/hg-prompt/ ~/contrib/hg-prompt/
+    \hg clone https://bitbucket.org/sjl/hg-prompt/ ~/contrib/hg-prompt/
   else
     \cd ~/contrib/hg-prompt
-    hg pull -u
+    \hg pull -u
     \cd -
   fi
 }
 
 grab_hgcfg() {
   if [ ! -d ~/contrib/hgcfg/.hg/ ]; then
-    hg clone https://bitbucket.org/bmearns/hgcfg ~/contrib/hgcfg
+    \hg clone https://bitbucket.org/bmearns/hgcfg ~/contrib/hgcfg
   else
     \cd ~/contrib/hgcfg
-    hg pull -u
+    \hg pull -u
     \cd -
   fi
 }
@@ -110,13 +110,14 @@ grab_powerline() {
 }
 
 grab_s3cmd() {
-  VER="s3cmd version 1.5.0-beta1"
+  VER="1.5.2"
+  VER_STRING="s3cmd version ${VER}"
   CURR_VER="$(s3cmd --version 2>&1)"
-  if [ "$CURR_VER" = "$VER" ]; then
+  if [ "$CURR_VER" = "$VER_STRING" ]; then
     echo "s3cmd currently installed and up to date"
   else
     echo "installing/upgrading s3cmd"
-    \sudo $PIP install -U s3cmd==1.5.0-alpha3
+    \sudo $PIP install -U s3cmd==$VER
   fi
 }
 
@@ -127,6 +128,17 @@ grab_composer() {
     \sudo \mv composer.phar /usr/local/bin/composer
     \cd -
   else
-    sudo composer self-update
+    \sudo composer self-update
+  fi
+}
+
+grab_icdiff() {
+  if ! command -v icdiff >/dev/null 2>&1 ; then
+    \git clone https://github.com/jeffkaufman/icdiff.git ~/contrib/icdiff
+    \ln -s ~/contrib/icdiff ~/bin/icdiff
+  else
+    \cd ~/contrib/icdiff
+    \git up
+    \cd -
   fi
 }
