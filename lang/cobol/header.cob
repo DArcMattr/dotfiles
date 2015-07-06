66c89d230f635463f40e4c952dfb3c88e18864df
David Arceneaux <david@davidthemachine.org>
2015-07-06 11:49:07 -0700

    From https://vivaldi.net/blogs/entry/some-simple-vim-settings-for-opencobol

---
+++ lang/cobol/header.cob
@@ -0,0 +1,35 @@
+OCOBOL >>SOURCE FORMAT IS FIXED
+
+      *> ***************************************************************
+
+      *> Author:    Brian Tiffin
+
+      *> Date:
+
+      *> Purpose:
+
+      *> Tectonics: cobc
+
+      *> ***************************************************************
+
+       identification division.
+
+       program-id. .
+
+
+
+       data division.
+
+       working-storage section.
+
+
+
+      *> ***************************************************************
+
+       procedure division.
+
+
+
+       goback.
+
+       end program .
