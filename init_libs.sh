7f48a4fe96bdf83f1f8f3695a27384f5def315e3
David Arceneaux <david@davidthemachine.org>
2015-10-30 19:08:52 -0700

    version bump for s3cmd

--- init_libs.sh
+++ init_libs.sh
@@ -76,7 +76,7 @@ grab_powerline() {
 }
 
 grab_s3cmd() {
-  VER="1.5.2"
+  VER="1.6.0"
   VER_STRING="s3cmd version ${VER}"
   CURR_VER="$(s3cmd --version 2>&1)"
   if [ "$CURR_VER" = "$VER_STRING" ]; then
