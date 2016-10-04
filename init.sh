252a73142da3be15347553fed6f328bdf9cdcc10
David Arceneaux <david@davidthemachine.org>
2016-10-04 15:55:46 -0700

    provide for .config dir

--- init.sh
+++ init.sh
@@ -2,17 +2,24 @@
 # only idempotent commands here so far
 # TODO: translate to ansible
 
-mkdir -p ~/contrib ~/.vim/syntax ~/.vim/bundle ~/bin
+mkdir -p ~/contrib ~/bin ~/.config/git ~/.config/nvim
 chmod 600 ~/dotfiles/sshconfig
+mv ~/.ssh/config ~/.ssh/config.orig
+ln -s ~/dotfiles/sshconfig ~/.ssh/config
+ln -s ~/dotfiles/xdg/git/config ~/.config/git/config
+ln -s ~/dotfiles/xdg/git/install ~/.config/git/install
+ln -s ~/dotfiles/xdg/git/attributes ~/.config/git/attributes
+ln -s ~/dotfiles/xdg/git/template ~/.config/git/template
+ln -s ~/dotfiles/xdg/nvim/init.vim ~/.config/nvim/init.vim
 
 find ~/dotfiles/symlinks/ -name ".*" -exec ln -sf "{}" "${HOME}" \;
 
 . ~/dotfiles/init_libs.sh
 
-grab_composer
 grab_hgcfg
 grab_hg_git
 grab_hg_prompt
+grab_composer
 grab_wp_cli
 grab_pips
 grab_sassc
