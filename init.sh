7283119edc2e74869848026dfd967ad1c1c6b1fb
David Arceneaux <david@davidthemachine.org>
2016-12-06 16:53:56 -0800

    run the grab_* functions in a subshell

--- init.sh
+++ init.sh
@@ -7,25 +7,26 @@ chmod 600 ~/dotfiles/sshconfig
 mv ~/.ssh/config ~/.ssh/config.orig
 ln -s ~/dotfiles/sshconfig ~/.ssh/config
 ln -s ~/dotfiles/xdg/git/config ~/.config/git/config
-ln -s ~/dotfiles/xdg/git/install ~/.config/git/install
 ln -s ~/dotfiles/xdg/git/attributes ~/.config/git/attributes
 ln -s ~/dotfiles/xdg/git/template ~/.config/git/template
 ln -s ~/dotfiles/xdg/nvim/init.vim ~/.config/nvim/init.vim
 
 find ~/dotfiles/symlinks/ -name ".*" -exec ln -sf "{}" "${HOME}" \;
 
-. ~/dotfiles/init_libs.sh
+(
+  . ~/dotfiles/init_libs.sh
 
-grab_hgcfg
-grab_hg_git
-grab_hg_prompt
-grab_composer
-grab_wp_cli
-grab_pips
-grab_sassc
-grab_tpm
-grab_nvm
-grab_autoenv
+  grab_hgcfg
+  grab_hg_git
+  grab_hg_prompt
+  grab_composer
+  grab_wp_cli
+  grab_pips
+  grab_sassc
+  grab_tpm
+  grab_nvm
+  grab_autoenv
+)
 
 composer global require 'phpmd/phpmd=*' \
   'squizlabs/php_codesniffer=*' \
