29904f9ab055426ddd7cf7978288c8ff65ce6009
David Arceneaux <david@davidthemachine.org>
2017-04-03 12:45:36 -0700

    also install via pip3

--- init_libs.sh
+++ init_libs.sh
@@ -84,6 +84,7 @@ grab_pips() {
   # I install mercurial via hg on Ubuntu, because the package version is out of
   # date and drags in all kinds of X dependencies
   sudo pip install -U psutil powerline-status s3cmd dulwich httpie neovim icdiff
+  sudo pip3 install -U psutil powerline-status s3cmd dulwich httpie neovim icdiff
 
   powerline_path="$(dirname "$(python -c 'import powerline; print (powerline.__file__)')")"
   if [ ! -d "${XDG_CONFIG_HOME:=$HOME/.config}/powerline" ]; then
