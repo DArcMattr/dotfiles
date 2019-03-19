ed29cef21b67aabab74928c5429d428939907407
David Arceneaux <david@davidthemachine.org>
2019-03-19 16:58:06 -0700

    sync with installed programs

--- helpers/debianish-update-alternatives.sh
+++ helpers/debianish-update-alternatives.sh
@@ -12,7 +12,6 @@ sudo update-alternatives \
   --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-7 \
   --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-7 \
   --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-7 \
-  --slave   /usr/bin/llvm-mcmarkup     llvm-mcmarkup    /usr/bin/llvm-mcmarkup-7 \
   --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-7 \
   --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-7 \
   --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-7 \
@@ -24,12 +23,23 @@ sudo update-alternatives \
   --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-7
 
 sudo update-alternatives \
-  --install /usr/bin/clang             clang             /usr/bin/clang-7 50 \
-  --slave   /usr/bin/clang++           clang++           /usr/bin/clang++-7 \
-  --slave   /usr/bin/clang-format      clang-format      /usr/bin/clang-format-7 \
-  --slave   /usr/bin/clang-format-diff clang-format-diff /usr/bin/clang-format-diff-7 \
-  --slave   /usr/bin/lldb              lldb              /usr/bin/lldb-7 \
-  --slave   /usr/bin/lldb-server       lldb-server       /usr/bin/lldb-server-7
+  --install /usr/bin/clang                    clang                    /usr/bin/clang-7 50 \
+  --slave   /usr/bin/clang++                  clang++                  /usr/bin/clang++-7 \
+  --slave   /usr/bin/clangd                   clangd                   /usr/bin/clangd-7 \
+  --slave   /usr/bin/clang-cl                 clang-cl                 /usr/bin/clang-cl-7 \
+  --slave   /usr/bin/clang-query              clang-query              /usr/bin/clang-query-7 \
+  --slave   /usr/bin/clang-check              clang-check              /usr/bin/clang-check-7 \
+  --slave   /usr/bin/clang-include-fixer      clang-include-fixer      /usr/bin/clang-include-fixer \
+  --slave   /usr/bin/clang-offload-bundler    clang-offload-bundler    /usr/bin/clang-offload-bundler-7 \
+  --slave   /usr/bin/clang-refactor           clang-refactor           /usr/bin/clang-refactor-7 \
+  --slave   /usr/bin/clang-import-test        clang-import-test        /usr/bin/clang-import-test-7 \
+  --slave   /usr/bin/clang-func-mapping       clang-func-mapping       /usr/bin/clang-func-mapping-7 \
+  --slave   /usr/bin/clang-apply-replacements clang-apply-replacements /usr/bin/clang-apply-replacements-7 \
+  --slave   /usr/bin/clang-change-namespace   clang-change-namespace   /usr/bin/clang-change-namespace-7 \
+  --slave   /usr/bin/clang-format             clang-format             /usr/bin/clang-format-7 \
+  --slave   /usr/bin/clang-format-diff        clang-format-diff        /usr/bin/clang-format-diff-7 \
+  --slave   /usr/bin/lldb                     lldb                     /usr/bin/lldb-7 \
+  --slave   /usr/bin/lldb-server              lldb-server              /usr/bin/lldb-server-7
 
 sudo update-alternatives \
    --install /usr/bin/go  go    /usr/lib/go-1.10/bin/go 50 \
