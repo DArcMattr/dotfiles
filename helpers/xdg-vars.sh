8e69f72ba9028ffe124e494687c2d5abe579178e
David Arceneaux <david@davidthemachine.org>
2019-05-08 14:32:15 -0700

    document global XDG vars

---
+++ helpers/xdg-vars.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# /etc/profile.d/xdg-vars.sh
+
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
