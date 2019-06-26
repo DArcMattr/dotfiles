6523847888700f7238c086c392060c592052f2e0
David Arceneaux <david@davidthemachine.org>
2019-06-26 12:35:37 -0700

    echo variables instead of rely on functions

--- xdg/fish/functions/fish_prompt.fish
+++ xdg/fish/functions/fish_prompt.fish
@@ -4,6 +4,7 @@ function fish_prompt
     jobs
   end
 
-  echo (date "+%Y/%m/%d %H:%I:%S") 
-  echo (echo $USER)'@'(echo -n $hostname)(set_color $fish_color_cwd) (pwd) (set_color normal) (__fish_vcs_prompt) '> '
+  echo '['(date "+%Y/%m/%d %H:%I:%S")']'
+  echo "<$USER@$hostname:$PWD>"
+  echo (__fish_vcs_prompt) ' '
 end
