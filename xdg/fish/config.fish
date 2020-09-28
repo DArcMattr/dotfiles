274a5f93658696c31c9f67073f5c8ce913c0aeff
David Arceneaux <david@davidthemachine.org>
2020-09-27 22:06:59 -0500

    experiment with porting zsh aliases/functions

--- xdg/fish/config.fish
+++ xdg/fish/config.fish
@@ -1,1 +1,3 @@
 starship init fish | source
+
+alias ll='LC_COLLATE=C ls -AalFh'
--- xdg/fish/functions/ls.fish
+++ xdg/fish/functions/ls.fish
@@ -1,13 +1,13 @@
 function ls
-  if \exa >/dev/null 2>&1
-    \exa --color=always
-  else if \ls --color=always -d . >/dev/null 2>&1
-    \ls --color=always
-  else if \ls -G -d . >/dev/null 2>&1 # colorls is main system ls already
-    \ls -G
+  if exa >/dev/null 2>&1
+    exa --color=always
+  else if ls --color=always -d . >/dev/null 2>&1
+    ls --color=always
+  else if ls -G -d . >/dev/null 2>&1 # colorls is main system ls already
+    ls -G
   else if \colorls -G -d . >/dev/null 2>&1
-    \colorls -G
+    colorls -G
   else # Solaris or other *NIX
-    \ls
+    ls
   end
 end
