eec3640de2be17fe033ea9144e13315ad6a482da
David Arceneaux <david@davidthemachine.org>
2023-12-11 08:23:09 -0600

    configure the new Go plugin

--- xdg/nvim/ftplugin/go.lua
+++ xdg/nvim/ftplugin/go.lua
@@ -1,16 +1,12 @@
 -- Golang
-local InsertOutsert = vim.api.nvim_create_augroup('GoFormat', {})
+local FormatSyncGrp = vim.api.nvim_create_augroup('GoFormat', {})
 
 require'lspconfig'.gopls.setup{}
-
-vim.g.go_term_mode = "split"
-vim.g.go_def_mode='gopls'
-vim.g.go_info_mode='gopls'
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
   callback = function()
     require('go.format').goimport()
   end,
-  group = format_sync_grp,
+  group = FormatSyncGrp,
 })
