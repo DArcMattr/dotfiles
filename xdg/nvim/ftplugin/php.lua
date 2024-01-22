599fefa1bf4c957878ef7068f7437e9a5e25ccb1
David Arceneaux <david@davidthemachine.org>
2024-01-22 13:37:48 -0600

    compute project_root for non-git projects

--- xdg/nvim/ftplugin/php.lua
+++ xdg/nvim/ftplugin/php.lua
@@ -1,10 +1,9 @@
 U.lspconfig.intelephense.setup {
   capabilities = U.capabilities,
   autostart = true,
-  root_dir = U.lspconfig.util.find_git_ancestor,
+  root_dir = U.lspconfig.util.root_pattern('.git', 'composer.json', 'index.php'),
 }
 
--- vim.opt_local.keywordprg = 'pman'
 vim.opt_local.iskeyword:append {'$'}
 vim.opt_local.suffixesadd:append {'.php'}
 vim.g.mta_filetypes.php = 1
