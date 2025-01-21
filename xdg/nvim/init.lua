be9342b57288df4210c8978b880e7b0cffb01526
David Arceneaux <david@davidthemachine.org>
2025-01-20 21:29:04 -0600

    configure for neovide

--- xdg/nvim/init.lua
+++ xdg/nvim/init.lua
@@ -508,6 +508,10 @@ vim.opt.wildignore:append {
   'build/*', 'dist/*'
 }
 
+if vim.g.neovide then
+  vim.o.guifont = 'Cascadia Code PL Light:h9'
+end
+
 if vim.fn.executable('rg') == 1 then
   vim.opt.grepprg='rg --vimgrep --no-heading --smart-case'
   vim.opt.grepformat='%f:%l:%c:%m,%f:%l:%m'
