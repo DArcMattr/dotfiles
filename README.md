defbbd4bcdf13c03605ab11973e8fffa8b3ca216
David Arceneaux <david@davidthemachine.org>
2025-05-07 13:10:43 -0500

    LLVM version bump

--- README.md
+++ README.md
@@ -34,24 +34,23 @@ curl -sS "https://packages.microsoft.com/keys/microsoft.asc" | \
 sudo add-apt-repository \
   "deb http://nginx.org/packages/mainline/ubuntu ${REL} nginx"
 sudo add-apt-repository \
-  "deb http://apt.llvm.org/${REL}/ llvm-toolchain-${REL}-19 main"
+  "deb http://apt.llvm.org/${REL}/ llvm-toolchain-${REL}-20 main"
 sudo add-apt-repository \
     "$(wget -qO- https://packages.microsoft.com/config/ubuntu/24.04/prod.list)" # XXX
-sudo apt install autossh bison btop build-essential clang-19 clang-19-doc \
-  clang-format-19 clang-tidy-19 clang-tools-19 clangd-19 cmake flex fswatch git \
-  git-extras golang jq libc++-19-dev-wasm32 libc++abi-19-dev-wasm32 \
-  libclang-19-dev libclang-common-19-dev libclang-rt-19-dev \
-  libclang-rt-19-dev-wasm32 libclang-rt-19-dev-wasm32 \
-  libclang-rt-19-dev-wasm64 libclang-rt-19-dev-wasm64 libclang1-19 \
-  libevent-dev libfuzzer-19-dev libgit2-dev libjansson-dev \
-  libllvm-19-ocaml-dev libllvm19 liblua5.3-dev liblzma-dev libncurses5-dev \
-  libnss3-tools libomp-19-dev libpcre3-dev libsqlite3-dev libssh2-1-dev \
-  libssl-dev libxml2-dev libyaml-dev lld-19 lldb-19 llvm-19 llvm-19-dev \
-  llvm-19-doc llvm-19-examples llvm-19-runtime lsyncd neovim nginx nodejs \
+sudo apt install autossh bison btop build-essential clang-20 clang-20-doc \
+  clang-format-20 clang-tidy-20 clang-tools-20 clangd-20 cmake flex fswatch git \
+  git-extras golang jq libc++-20-dev-wasm32 libc++abi-20-dev-wasm32 \
+  libclang-20-dev libclang-common-20-dev libclang-rt-20-dev \
+  libclang-rt-20-dev-wasm32 libclang-rt-20-dev-wasm64 libclang1-20 
+  libevent-dev libfuzzer-20-dev libgit2-dev libjansson-dev \
+  libllvm-20-ocaml-dev libllvm19 liblua5.3-dev liblzma-dev libncurses5-dev \
+  libnss3-tools libomp-20-dev libpcre3-dev libsqlite3-dev libssh2-1-dev \
+  libssl-dev libxml2-dev libyaml-dev lld-20 lldb-20 llvm-20 llvm-20-dev \
+  llvm-20-doc llvm-20-examples llvm-20-runtime lsyncd neovim nginx nodejs \
   php-common php-gd php-imagick php-mbstring php-memcache php-pear php-xdebug \
   php-xml php-zip php8.4-cli php8.4-curl php8.4-dev php8.4-fpm php8.4-imap \
   php8.4-mysql php8.4-opcache php8.4-readline php8.4-soap php8.4-sqlite3 \
-  php8.4-xml pv python-is-python3 python3-clang-19 python3-dev python3-venv \
+  php8.4-xml pv python-is-python3 python3-clang-20 python3-dev python3-venv \
   ruby-dev shellcheck sqlcmd tidy wget wslu xcape xsel zlib1g-dev zsh
 
 git clone https://github.com/darcmattr/dotfiles ~/dotfiles
--- helpers/debianish-update-alternatives.sh
+++ helpers/debianish-update-alternatives.sh
@@ -1,47 +1,47 @@
 #!/usr/bin/env sh
 
 sudo update-alternatives \
-  --install /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-19 200 \
-  --slave   /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-19 \
-  --slave   /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-19 \
-  --slave   /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-19 \
-  --slave   /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-19 \
-  --slave   /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-19 \
-  --slave   /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-19 \
-  --slave   /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-19 \
-  --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-19 \
-  --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-19 \
-  --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-19 \
-  --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-19 \
-  --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-19 \
-  --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-19 \
-  --slave   /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-19 \
-  --slave   /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-19 \
-  --slave   /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-19 \
-  --slave   /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-19 \
-  --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-19 \
-  --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-19
+  --install /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-20 200 \
+  --slave   /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-20 \
+  --slave   /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-20 \
+  --slave   /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-20 \
+  --slave   /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-20 \
+  --slave   /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-20 \
+  --slave   /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-20 \
+  --slave   /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-20 \
+  --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-20 \
+  --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-20 \
+  --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-20 \
+  --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-20 \
+  --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-20 \
+  --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-20 \
+  --slave   /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-20 \
+  --slave   /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-20 \
+  --slave   /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-20 \
+  --slave   /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-20 \
+  --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-20 \
+  --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-20
 
 sudo update-alternatives \
-  --install /usr/bin/clang                    clang                    /usr/bin/clang-19 50 \
-  --slave   /usr/bin/clang++                  clang++                  /usr/bin/clang++-19 \
-  --slave   /usr/bin/clangd                   clangd                   /usr/bin/clangd-19 \
-  --slave   /usr/bin/clang-cl                 clang-cl                 /usr/bin/clang-cl-19 \
-  --slave   /usr/bin/clang-query              clang-query              /usr/bin/clang-query-19 \
-  --slave   /usr/bin/clang-check              clang-check              /usr/bin/clang-check-19 \
-  --slave   /usr/bin/clang-include-fixer      clang-include-fixer      /usr/bin/clang-include-fixer-19 \
-  --slave   /usr/bin/clang-offload-bundler    clang-offload-bundler    /usr/bin/clang-offload-bundler-19 \
-  --slave   /usr/bin/clang-refactor           clang-refactor           /usr/bin/clang-refactor-19 \
-  --slave   /usr/bin/clang-apply-replacements clang-apply-replacements /usr/bin/clang-apply-replacements-19 \
-  --slave   /usr/bin/clang-change-namespace   clang-change-namespace   /usr/bin/clang-change-namespace-19 \
-  --slave   /usr/bin/clang-format             clang-format             /usr/bin/clang-format-19 \
-  --slave   /usr/bin/clang-format-diff        clang-format-diff        /usr/bin/clang-format-diff-19 \
-  --slave   /usr/bin/lldb                     lldb                     /usr/bin/lldb-19 \
-  --slave   /usr/bin/lldb-server              lldb-server              /usr/bin/lldb-server-19 \
-  --slave   /usr/bin/lldb-vscode              lldb-vscode              /usr/bin/lldb-vscode-19
+  --install /usr/bin/clang                    clang                    /usr/bin/clang-20 50 \
+  --slave   /usr/bin/clang++                  clang++                  /usr/bin/clang++-20 \
+  --slave   /usr/bin/clangd                   clangd                   /usr/bin/clangd-20 \
+  --slave   /usr/bin/clang-cl                 clang-cl                 /usr/bin/clang-cl-20 \
+  --slave   /usr/bin/clang-query              clang-query              /usr/bin/clang-query-20 \
+  --slave   /usr/bin/clang-check              clang-check              /usr/bin/clang-check-20 \
+  --slave   /usr/bin/clang-include-fixer      clang-include-fixer      /usr/bin/clang-include-fixer-20 \
+  --slave   /usr/bin/clang-offload-bundler    clang-offload-bundler    /usr/bin/clang-offload-bundler-20 \
+  --slave   /usr/bin/clang-refactor           clang-refactor           /usr/bin/clang-refactor-20 \
+  --slave   /usr/bin/clang-apply-replacements clang-apply-replacements /usr/bin/clang-apply-replacements-20 \
+  --slave   /usr/bin/clang-change-namespace   clang-change-namespace   /usr/bin/clang-change-namespace-20 \
+  --slave   /usr/bin/clang-format             clang-format             /usr/bin/clang-format-20 \
+  --slave   /usr/bin/clang-format-diff        clang-format-diff        /usr/bin/clang-format-diff-20 \
+  --slave   /usr/bin/lldb                     lldb                     /usr/bin/lldb-20 \
+  --slave   /usr/bin/lldb-server              lldb-server              /usr/bin/lldb-server-20 \
+  --slave   /usr/bin/lldb-vscode              lldb-vscode              /usr/bin/lldb-vscode-20
 
 sudo update-alternatives \
-  --install  /usr/bin/cc                      cc                       /usr/bin/clang-19 50
+  --install  /usr/bin/cc                      cc                       /usr/bin/clang-20 50
 
 sudo update-alternatives \
-  --install  /usr/bin/cpp                     cpp                      /usr/bin/clang++-19 50
+  --install  /usr/bin/cpp                     cpp                      /usr/bin/clang++-20 50
