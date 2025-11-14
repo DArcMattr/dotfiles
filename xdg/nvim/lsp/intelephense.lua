bc25439aaf186a4beee46d9f799d8b62f989cb7d
David Arceneaux <david@davidthemachine.org>
2025-11-13 21:25:25 -0600

    let intelephense handle big files

--- xdg/nvim/lsp/intelephense.lua
+++ xdg/nvim/lsp/intelephense.lua
@@ -3,6 +3,9 @@ return {
   capabilities = require('cmp_nvim_lsp').default_capabilities(),
   root_markers = { '.git', 'composer.json', 'index.php' },
   settings = {
+    intelephense = {
+      maxFileSize = 5000000, -- 5MB (default is 1MB)
+    },
     codeLens = {
       references = {
         enable = true
