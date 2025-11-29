f59478ea04482f71e1a179c8a9e85fc6d9ecdd46
David Arceneaux <david@davidthemachine.org>
2025-11-28 19:47:32 -0600

    ongoing tweaking, will stop for the evening and sleep on it

--- xdg/wezterm/wezterm.lua
+++ xdg/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ config.color_scheme = 'Campbell (Gogh)'
 config.exit_behavior = 'CloseOnCleanExit'
 config.default_cursor_style = 'BlinkingBlock'
 config.enable_scroll_bar = true
+config.disable_default_key_bindings = true
 config.font = wezterm.font_with_fallback{'Cascadia Code PL', 'Twemoji'}
 config.font_size = 9.0
 config.hide_tab_bar_if_only_one_tab = false
@@ -23,42 +24,45 @@ config.keys = {
   { key = 'v', mods = 'LEADER',       action=act{SplitHorizontal={domain='CurrentPaneDomain'}}},
   { key = 'o', mods = 'LEADER',       action='TogglePaneZoomState' },
   { key = 'z', mods = 'LEADER',       action='TogglePaneZoomState' },
+  { key = 'p', mods = 'LEADER',       action=act.ActivateCommandPalette },
   { key = 'c', mods = 'LEADER',       action=act{SpawnTab='CurrentPaneDomain'}},
   { key = 'h', mods = 'LEADER',       action=act{ActivatePaneDirection='Left'}},
   { key = 'j', mods = 'LEADER',       action=act{ActivatePaneDirection='Down'}},
   { key = 'k', mods = 'LEADER',       action=act{ActivatePaneDirection='Up'}},
   { key = 'l', mods = 'LEADER',       action=act{ActivatePaneDirection='Right'}},
-  { key = 'LeftArrow', mods = 'LEADER',  action=act{ActivatePaneDirection='Left'}},
-  { key = 'DownArrow', mods = 'LEADER',  action=act{ActivatePaneDirection='Down'}},
-  { key = 'UpArrow', mods = 'LEADER',    action=act{ActivatePaneDirection='Up'}},
+  { key = 'LeftArrow',  mods = 'LEADER',  action=act{ActivatePaneDirection='Left'}},
+  { key = 'DownArrow',  mods = 'LEADER',  action=act{ActivatePaneDirection='Down'}},
+  { key = 'UpArrow',    mods = 'LEADER', action=act{ActivatePaneDirection='Up'}},
   { key = 'RightArrow', mods = 'LEADER', action=act{ActivatePaneDirection='Right'}},
-  { key = 'PageUp', mods = 'ALT',     action=act.ActivateTabRelative(1)},
-  { key = 'PageDown', mods = 'ALT',     action=act.ActivateTabRelative(-1)},
+  { key = 'PageUp',   mods = 'ALT',   action=act.ActivateTabRelative(1)},
+  { key = 'PageDown', mods = 'ALT',   action=act.ActivateTabRelative(-1)},
   { key = 'H', mods = 'LEADER|SHIFT', action=act{AdjustPaneSize={'Left', 5}}},
   { key = 'J', mods = 'LEADER|SHIFT', action=act{AdjustPaneSize={'Down', 5}}},
   { key = 'K', mods = 'LEADER|SHIFT', action=act{AdjustPaneSize={'Up', 5}}},
   { key = 'L', mods = 'LEADER|SHIFT', action=act{AdjustPaneSize={'Right', 5}}},
-  { key = '1', mods = 'LEADER',       action=act{ActivateTab=0}},
-  { key = '2', mods = 'LEADER',       action=act{ActivateTab=1}},
-  { key = '3', mods = 'LEADER',       action=act{ActivateTab=2}},
-  { key = '4', mods = 'LEADER',       action=act{ActivateTab=3}},
-  { key = '5', mods = 'LEADER',       action=act{ActivateTab=4}},
-  { key = '6', mods = 'LEADER',       action=act{ActivateTab=5}},
-  { key = '7', mods = 'LEADER',       action=act{ActivateTab=6}},
-  { key = '8', mods = 'LEADER',       action=act{ActivateTab=7}},
-  { key = '9', mods = 'LEADER',       action=act{ActivateTab=8}},
   { key = '&', mods = 'LEADER|SHIFT', action=act{CloseCurrentTab={confirm=true}}},
   { key = 'd', mods = 'LEADER',       action=act{CloseCurrentPane={confirm=true}}},
   { key = 'x', mods = 'LEADER',       action=act{CloseCurrentPane={confirm=true}}},
   { key = '[', mods = 'LEADER',       action='ActivateCopyMode'},
+  { key = '=', mods = 'CTRL',         action=act.IncreaseFontSize},
+  { key = '-', mods = 'CTRL',         action=act.DecreaseFontSize},
+  { key = '0', mods = 'LEADER|CTRL',  action=act.ResetFontSize},
 }
+for i = 0, 9 do
+	table.insert(config.keys, {
+		key = tostring((i + 1) % 10),
+		mods = 'LEADER',
+		action = wezterm.action.ActivateTab(i),
+	})
+end
 config.leader = { key='b', mods='ALT' }
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'NONE'
 
-wezterm.on("update-right-status", function(window, _)
+wezterm.on("update-status", function(window, _)
 	local SOLID_LEFT_ARROW = ""
 	local prefix = ""
+	local date = wezterm.strftime '%Y-%m-%d %H:%M:%S'
 
 	if window:leader_is_active() then
 		prefix = " " .. utf8.char(0x1f47e) .. " "
@@ -73,12 +77,18 @@ wezterm.on("update-right-status", function(window, _)
         end
 	end
 
-
 	window:set_left_status(wezterm.format{
 		{ Background = { Color = "#b7bdf8" } },
 		{ Text = prefix },
 		ARROW_FOREGROUND,
 		{ Text = SOLID_LEFT_ARROW },
+	})
+
+	window:set_right_status(wezterm.format{
+		{ Background = { Color = "#dd3333" } },
+		{ Foreground = { Color = "#eeeeee" } },
+		{ Text = date },
+    { }
 	})
 end)
 
