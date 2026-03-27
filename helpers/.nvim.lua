2d006d9f8020cb7fc53a1235c06023cec5e25854
David Arceneaux <david@davidthemachine.org>
2026-03-27 17:34:21 -0500

    track local machine stuffs

--- helpers/.nvim.lua
+++ helpers/.nvim.lua
@@ -1,4 +1,5 @@
 local LocalPHP = vim.api.nvim_create_augroup('LocalPHP', { clear = true })
+local bin = vim.fn.getcwd() .. '/bin/'
 
 U.dap.configurations.php = {
   {
@@ -23,13 +24,13 @@ U.dap.configurations.javascript = {
 }
 
 vim.g.php_version_id = 80300
-vim.opt.textwidth = 90
 vim.opt.includeexpr = vim.fn.substitute(vim.v.fname, '^/', '', '')
+vim.keymap.set({'n','t'}, '<Leader>cc', '<Cmd>ClaudeCodeContinue<Cr>', { desc = 'Toggle Claude Code (continue)'})
 
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufEnter', 'BufWinEnter'}, {
     pattern = '*',
     group = LocalPHP,
-    callback = function(ev)
+    callback = function(_)
         vim.opt.path = { '.', vim.lsp.buf.list_workspace_folders()[1] }
     end
 })
@@ -37,15 +38,43 @@ vim.api.nvim_create_autocmd({'BufNewFile', 'BufEnter', 'BufWinEnter'}, {
 vim.api.nvim_create_autocmd({'LspAttach'}, {
     pattern = '*.php',
     group = LocalPHP,
-    callback = function(ev)
-        vim.opt.tags:append { vim.lsp.buf.list_workspace_folders()[1] .. '/php.tags' }
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local root_dir = client ~= nil and client.config.root_dir
+        if root_dir ~= nil then
+            vim.opt.tags:append { vim.lsp.buf.list_workspace_folders()[1] .. '/php.tags' }
+        end
     end
+})
+
+vim.api.nvim_create_autocmd({'LspAttach'}, {
+    pattern = '*.php',
+    group = LocalPHP,
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.name == 'intelephense' then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings or {}, {
+                intelephense = {
+                    diagnostics = {
+                        unusedSymbols = false,
+                    },
+                },
+            })
+            client:notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd({'LspAttach'}, {
     pattern = '*.js',
     group = LocalPHP,
-    callback = function(ev)
+    callback = function(_)
         vim.opt.tags:append { vim.lsp.buf.list_workspace_folders()[1] .. '/js.tags' }
     end
+})
+
+U.utils.add_null_ls_sources({
+  U.null_ls.builtins.diagnostics.phpcs.with({ command = bin .. 'phpcs' }),
+  U.null_ls.builtins.diagnostics.phpstan.with({ command = bin .. 'phpstan' }),
+  U.null_ls.builtins.formatting.phpcbf.with({ command = bin .. 'phpcbf' }),
 })
